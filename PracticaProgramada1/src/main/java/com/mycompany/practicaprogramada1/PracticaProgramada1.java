d566af17adcfc9050498f82214076678b6bfa0d4
jaime2804 <113743405+jaime2804@users.noreply.github.com>
2022-09-22 22:17:37 -0600

    Add files via upload

---
+++ PracticaProgramada1/src/main/java/com/mycompany/practicaprogramada1/PracticaProgramada1.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.practicaprogramada1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ExtremeTech Sc
+ */
+public class PracticaProgramada1 {
+
+    public static void main(String[] args) {
+
+/*                              Ejercicio 1      
+        int numero;
+        String lecturaNumero;
+
+        lecturaNumero = JOptionPane.showInputDialog("Digite un numero del 1 al 12: ");
+        numero = Integer.parseInt(lecturaNumero);
+
+        switch (numero) {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Mes: Enero ");
+                JOptionPane.showMessageDialog(null, "Primer Cuatrimestre ");
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Mes: Febrero ");
+                JOptionPane.showMessageDialog(null, "Primer Cuatrimestre ");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Mes: Marzo ");
+                JOptionPane.showMessageDialog(null, "Primer Cuatrimestre ");
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Mes: Abril ");
+                JOptionPane.showMessageDialog(null, "Primer Cuatrimestre ");
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "Mes: Mayo ");
+                JOptionPane.showMessageDialog(null, "Segundo Cuatrimestre ");
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null, "Mes: Junio ");
+                JOptionPane.showMessageDialog(null, "Segundo Cuatrimestre ");
+                break;
+            case 7:
+                JOptionPane.showMessageDialog(null, "Mes: Julio ");
+                JOptionPane.showMessageDialog(null, "Segundo Cuatrimestre ");
+                break;
+            case 8:
+                JOptionPane.showMessageDialog(null, "Mes: Agosto ");
+                JOptionPane.showMessageDialog(null, "Segundo Cuatrimestre ");
+                break;
+            case 9:
+                JOptionPane.showMessageDialog(null, "Mes: Septiembre ");
+                JOptionPane.showMessageDialog(null, "Tercer Cuatrimestre ");
+                break;
+            case 10:
+                JOptionPane.showMessageDialog(null, "Mes: Octubre ");
+                JOptionPane.showMessageDialog(null, "Tercer Cuatrimestre ");
+                break;
+            case 11:
+                JOptionPane.showMessageDialog(null, "Mes: Noviembre ");
+                JOptionPane.showMessageDialog(null, "Tercer Cuatrimestre ");
+                break;
+            case 12:
+                JOptionPane.showMessageDialog(null, "Mes: Diciembre ");
+                JOptionPane.showMessageDialog(null, "Tercer Cuatrimestre ");
+                break;
+
+        }
+ */
+ /*                             Ejercicio 2               
+        int edad;
+        String lecturaEdad;
+
+        lecturaEdad = JOptionPane.showInputDialog("Digite la edad: ");
+        edad = Integer.parseInt(lecturaEdad);
+
+        if (edad <= 5) {
+            JOptionPane.showMessageDialog(null, "Primera infancia");
+        }
+
+        if (edad >= 6 && edad <= 11) {
+            JOptionPane.showMessageDialog(null, "Infancia");
+        }
+        if (edad >= 12 && edad <= 18) {
+            JOptionPane.showMessageDialog(null, "Adolescencia");
+        }
+        if (edad >= 19 && edad <= 30) {
+            JOptionPane.showMessageDialog(null, "Juventud");
+        }
+        if (edad >= 30 && edad <= 59) {
+            JOptionPane.showMessageDialog(null, "Adultez");
+
+        }
+        if (edad > 60) {
+            JOptionPane.showMessageDialog(null, "Persona Mayor");
+        }
+*/
+    }
+
+}
